34d58c7c4407b3412edeb8122d8bd7a3a6c4f377
SeyiPerfect1 <adeegbeoluseyi@.com>
2022-11-06 11:03:43 +0100

    complete: reading_time algorithm

--- src/utils/reading_time.js
+++ src/utils/reading_time.js
@@ -1,8 +1,12 @@
 //reading time is estimated to be 130wpm(words per minutes) averagely
+/*
+@{params} string
+@{return} Number
+**/
 const readingTime = async (post) => {
   const wordCount = post.split(" ").length;
   const totalTime = (wordCount * 1) / 130;
-  return `${ Math.ceil(totalTime) } min`;
+  return `${Math.ceil(totalTime)}`;
 };
 
 module.exports = readingTime;
