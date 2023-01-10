1ce32fdc3462fae4f83088166aafd2ee44811b8f
SeyiPerfect1 <adeegbeoluseyi@.com>
2023-01-10 01:03:41 +0100

    chore: refactored controllers to follw the new api code stucture

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,290 +1,121 @@
-const Post = require("../models/post.models");
-const User = require("../models/user.models");
-const dayjs = require("dayjs");
-const readingTime = require("../utils/reading_time");
+const getManyPosts = require('../services/post/getMany');
+const getOnePost = require('../services/post/getOne');
+const createnewPost = require('../services/post/create');
+const updateOnePost = require('../services/post/update');
+const publishOnePost = require('../services/post/publish');
+const deleteOnePost = require('../services/post/delete');
+const { NotFoundError, AuthorizationError } = require('../lib/errors/index');
+const postSchema = require('../validation/post.validation');
 
-//function to get posts
-const getPosts = async (req, res, next) => {
-  //destructure query parameters
-  const { page, limit, id, order_by, tags, title, author, start, end } =
-    req.query;
+class PostController {
+  //  function to get posts
+  static getPosts = async (req, res, next) => {
+    try {
+    //  check if only one else set aggregation pipeline to achieve and filtering
+      const post = await getManyPosts(req.query);
 
-  //deduct 1 from the page no before multiplying with the limit value
-  //if page is not provided in query, set to 0
-  const pageNO = parseInt(page) - 1 || 0;
+      res.status(200).json({
+        page: post?.pageNO,
+        limit: post?.postLimit,
+        message: post?.post
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 
-  const findQuery = [];
-  //check if author or title or tag is not provided in query params
-  //if none is provided pass in an empty string
-  if (!author && !title && !tags && !id) {
-    findQuery.push({});
-  }
-  //if author is provided as a query params
-  //set the query in an object and push into the findquery array
-  if (author) {
-    findQuery.push({ "author.username": author });
-  }
+  //  function to get a specific post
+  static getPost = async (req, res, next) => {
+    try {
+      const post = await getOnePost(req.params);
 
-  //check for id
-  if (id) {
-    findQuery.push({ id: { $regex: `${id}`, $options: "i" } });
-  }
+      if (post.state === 'published') {
+        res.status(200).json({
+          message: post
+        });
+      } else {
+        throw new NotFoundError('you can only view published posts or draft post created by you!!!');
+      };
+    } catch (err) {
+      next(err);
+    }
+  };
 
-  //if title is provided as a query params
-  //set the query to match the title as a regex in an object
-  //and push into the findquery array
-  if (title) {
-    findQuery.push({ title: { $regex: `${title}`, $options: "i" } });
-  }
-  //if tags is/are provided as a query params
-  //set the query to match document that match one/more of the tag(s) in an object
-  //and push into the findquery array
-  //tags should be separated by space or mysql standard '+' in the url
-  if (tags) {
-    const tagQuery = tags.split(" ");
-    findQuery.push({ tags: { $in: tagQuery } });
-  }
-
-  //query where published date is in the range start to end date
-  //where start and end is in the format dd-mm-yyyy
-  //and push to the findQuery array
-  if (start && end) {
-    const startDate = dayjs(start).format("YYYY-MM-DD");
-    const endDate = dayjs(end).format("YYYY-MM-DD");
-    findQuery.push({
-      publishedAt: {
-        $gt: dayjs(startDate).startOf("day"),
-        $lt: dayjs(endDate).endOf("day"),
-      },
-    });
-  }
-
-  //sorting posts according to sort query given in query parameter
-  //by default, each query is sorted by publishedDate in descending order
-  //A sample url pattern for the sort query is:
-  //http://myapp.com/books?author+asc&datePublished+desc&title=loremipsum
-  //where "," separates the sort attributes
-  //while "+" separtes the field used for the sort and the sorting value
-  const sortQuery = {};
-
-  if (order_by) {
-    sortAttributes = order_by.split(",");
-
-    for (const attribute of sortAttributes) {
-      const sortField = attribute.split(" ")[0];
-      const sortValue = attribute.split(" ")[1];
-      if (sortValue === "asc") {
-        sortQuery[sortField] = 1;
-        if (sortField === "publishedAt") {
-          sortQuery[sortField] = -1;
-        }
+  //  function to create new post
+  static createPost = async (req, res, next) => {
+    try {
+      try {
+        const value = await postSchema.validateAsync(req.body);
+        console.log(value);
+      } catch (err) {
+        next(err);
       }
 
-      if (sortValue === "desc") {
-        sortQuery[sortField] = -1;
-        if (sortField === "publishedAt") {
-          sortQuery[sortField] = 1;
-        }
+      const post = await createnewPost(req, req.body);
+      res.status(201).json({
+        message: 'post created successfully',
+        title: post.title,
+        description: post.description,
+        readCount: post.readCount,
+        readingTime: post.readingTime,
+        state: post.state,
+        tags: post.tags,
+        slug: post.slug
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  //  function to update post
+  static updatePost = async (req, res, next) => {
+    try {
+    //   try {
+    //     const value = await postSchema.validateAsync(req.body);
+    //     console.log(value);
+    //   } catch (err) {
+    //     next(err);
+    //   }
+
+      await updateOnePost(req, req.params, req.body);
+
+      res.status(200).json({
+        message: 'post updated successfully'
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // function to publish a post
+  static publishPost = async (req, res, next) => {
+    try {
+      const post = await publishOnePost(req.params);
+
+      if (post.author.email === req.user.email) {
+        res.status(200).json({
+          message: 'Post published successfully'
+        });
+      } else if (post.author.email !== req.user.email) {
+        throw new AuthorizationError('user is not the owner of post, user cannot publish post');
       }
+    } catch (err) {
+      next(err);
     }
-  }
-  if (!order_by) {
-    sortQuery.publishedAt = -1;
-  }
+  };
 
-  try {
-    //check if only one else set agrregation pipeline to achieve and filtering
-    const post = await Post.find({ $or: findQuery, state: "published" })
-      .skip(pageNO * limit || pageNO * 20)
-      .limit(limit || 20)
-      .sort(sortQuery)
-      .select({ __v: false });
-    res.status(200).json({
-      result: `${post.length} result(s) found`,
-      message: post,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  // function to delete a post
+  static deletePost = async (req, res, next) => {
+    try {
+      await deleteOnePost(req, req.params);
 
-//function to get a specific post
-const getPost = async (req, res, next) => {
-  const { slug } = req.params;
-  try {
-    const post = await Post.findOne({ slug: slug.toLowerCase() });
-    if (!post) {
-      res.json({
-        message: "post not found",
-      });
-    }
-    post.readCount = post.readCount + 1;
-    await post.save();
-    console.log(req.user);
-
-    if (post.state === "published") {
       res.status(200).json({
-        message: post,
+        message: 'Post deleted successfully'
       });
-    } else if (post.state === "draft") {
-      res.status(200).json({
-        message: "post not found",
-      });
-    } else {
-      //if post slug is not found
-      res.status(404).json({
-        message: "post not found",
-      });
+    } catch (err) {
+      next(err);
     }
-  } catch (err) {
-    next(err);
-  }
+  };
 };
 
-//function to create new post
-const createPost = async (req, res, next) => {
-  const newPost = req.body;
-
-  //seperate tags(coming as strings) into array
-  if (newPost.tags) {
-    const tags = req.body.tags.replace(/\s/g, "").split(",");
-    newPost.tags = tags;
-  }
-
-  //user should not be able to set readCount and readingTime of post
-  if (newPost.readCount) {
-    delete newPost.readCount;
-  }
-  //readingTime is calculated automatically from body
-  if (newPost.readingTime) {
-    delete newPost.readingTime;
-  }
-  //state is default to draft
-  if (newPost.state) {
-    newPost.state = newPost.state.toLowerCase();
-    newPost.publishedAt = Date.now();
-  }
-
-  //calculate reading time
-  const reading_time = await readingTime(newPost.body);
-  newPost.readingTime = reading_time;
-  try {
-    //add author
-    const user = await User.findOne({ email: req.user.email }).select({
-      password: false,
-      __v: false,
-      posts: false,
-      id: false,
-      updatedAt: false,
-    });
-    newPost["author"] = user;
-    const post = await Post.create(newPost);
-    await User.updateOne({ _id: req.user._id }, { $push: { posts: post } });
-    res.status(201).json({
-      message: "post created successfully",
-      title: post.title,
-      description: post.description,
-      readCount: post.readCount,
-      readingTime: post.readingTime,
-      state: post.state,
-      tags: post.tags,
-      slug: post.slug,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-//function to update post
-const updatePost = async (req, res, next) => {
-  const { slug } = req.params;
-  const postUpdate = req.body;
-
-  //seperate tags(coming as strings) into array
-  if (postUpdate.tags) {
-    const tags = req.body.tags.replace(/\s/g, "").split(",");
-    postUpdate.tags = tags;
-  }
-
-  //readCount and radingTime should can not be updated by the user
-  if (postUpdate.readCount) {
-    delete postUpdate.readCount;
-  }
-  if (postUpdate.readingTime) {
-    delete postUpdate.readingTime;
-  }
-  //check if update consists of changing state to pushided.
-  //if true, add publishedAt
-  if (postUpdate.state) {
-    postUpdate.state = postUpdate.state.toLowerCase();
-    postUpdate.publishedAt = Date.now();
-  }
-
-  //calculate reading time
-  if (postUpdate.body) {
-    const reading_time = await readingTime(postUpdate.body);
-    postUpdate.readingTime = reading_time;
-  }
-  postUpdate.updatedAt = Date.now();
-  try {
-    const post = await Post.findOne({ slug: slug.toLowerCase() });
-    //create new slug from title
-    if (postUpdate.title) {
-      const titleSlug = postUpdate.title.replaceAll(" ", "-").toLowerCase();
-      const newSlug = titleSlug + "-" + post._id.toString();
-      postUpdate.slug = newSlug;
-    }
-    //create new description, if no description was added on while updating title
-    if (!postUpdate.description && postUpdate.title) {
-      postUpdate.description = postUpdate.title;
-    }
-    if (post.author.email === req.user.email) {
-      await Post.updateOne({ slug: slug.toLowerCase() }, { $set: postUpdate });
-      res.status(200).json({
-        message: "post updated successfully",
-        publishedAt: post.publishedAt,
-      });
-    } else {
-      res.status(401).json({
-        message: "user is not the owner of post, user cannot update post",
-      });
-    }
-  } catch (err) {
-    next(err);
-  }
-};
-
-const deletePost = async (req, res, next) => {
-  const { slug } = req.params;
-
-  try {
-    const user = await User.findOne({ _id: req.user._id });
-    const post = await Post.findOne({ slug: slug.toLowerCase() });
-
-    const index = user.posts.indexOf(post._id);
-    if (post.author.email === req.user.email) {
-      await post.deleteOne({ slug: slug });
-      //remove deleted post id from user.posts
-      const index = user.posts.indexOf(post._id);
-      user.posts.splice(index, 1);
-      await user.save();
-      res.status(200).json({
-        message: "Post deleted successfully",
-      });
-    } else {
-      res.status(401).json({
-        message: "user is not the owner of post, user cannot delete post",
-      });
-    }
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports = {
-  getPosts,
-  createPost,
-  updatePost,
-  getPost,
-  deletePost,
-};
+module.exports = PostController;
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,88 +1,74 @@
-const User = require("../models/user.models");
-const Post = require("../models/post.models");
+const getUser = require('../services/user/get');
+const updateUser = require('../services/user/update');
+const deletetUser = require('../services/user/delete');
+const getUserAllPosts = require('../services/user/getPosts');
+const getUserAllComments = require('../services/user/getComments');
+// const userSchema = require('../validation/user.validation');
 
-//function to retrieve details of a specific user
-const getUserDetails = async (req, res, next) => {
-  const { state } = req.query;
-  const { username } = req.params;
-  try {
-    const user = await User.findOne({ username: username }).select({
-      password: false,
-      __v: false,
-      posts: false,
-      _id: false,
-      id: false,
-    });
+class UserController {
+  //  function to retrieve details of a specific user
+  static getUserDetails = async (req, res, next) => {
+    try {
+      const user = await getUser(req, req.params, req.query);
 
-    if (!user) {
-      res.redirect("/")
-    }
-
-    else if (user.email === req.user.email) {
-      const filter = { "author.username": username };
-      if (state) {
-        filter.state = state;
-      }
-      const posts = await Post.find(filter);
       res.status(200).json({
-        message: `${username} has ${posts.length} post(s)`,
-        post: posts,
+        user
       });
-    } else {
-      const posts = await Post.find({
-        "author.username": username,
-        state: "published",
-      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  // get user posts
+  static getUserPosts = async (req, res, next) => {
+    try {
+      const post = await getUserAllPosts(req, req.params, req.query);
       res.status(200).json({
-        message: `${username} has ${posts.length} post(s)`,
-        post: posts,
+        message: `${post.username} has ${post.posts.length || 0} post(s)`,
+        posts: post.posts
       });
+    } catch (err) {
+      next(err);
     }
-  } catch (err) {
-    next(err);
-  }
-};
+  };
 
-const updateUser = async (req, res, next) => {
-  const userDetails = req.body;
-  //take in user's new update and add it to a new object
-  //exclude password and email in case the user added it as input
-  //password and email update require special methods
-  const newDetails = {};
-  for (const detail in userDetails) {
-    if (detail != "email" && detail != "password" && detail != "posts") {
-      newDetails[detail] = userDetails[detail];
+  //  get user comments return commet
+  static getUserComments = async (req, res, next) => {
+    try {
+      const comments = await getUserAllComments(req, req.params, req.query);
+
+      res.status(200).json({
+        message: `${comments.username} has ${comments.comments.length || 0} comment(s)`,
+        comments: comments.comments
+      });
+    } catch (err) {
+      next(err);
     }
-  }
-  newDetails.updatedAt = Date.now();
-  try {
-    const user = await User.findOneAndUpdate(
-      { email: req.user.email },
-      { $set: newDetails },
-      { new: true }
-    ).select({ password: false, __v: false });
-    res.status(200).json({
-      message: "user details updated successfully",
-    });
-  } catch (err) {
-    next(err);
-  }
+  };
+
+  static updateUserDetails = async (req, res, next) => {
+    try {
+      await updateUser(req, req.params, req.body);
+      res.status(200).json({
+        message: 'user details updated successfully'
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  //  delte account
+  static deleteUserDetails = async (req, res, next) => {
+    try {
+      await deletetUser(req, req.params);
+
+      res.status(200).json({
+        message: 'user deleted successfuly'
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 };
 
-//delte account
-const deleteUser = async (req, res, next) => {
-  try {
-    await User.findOneAndDelete({ email: req.user.email });
-    res.status(200).json({
-      message: "user deleted successfuly",
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports = {
-  getUserDetails,
-  updateUser,
-  deleteUser,
-};
+module.exports = UserController;
