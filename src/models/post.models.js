9f696d8f51033eab8c6a5d32251cfa8e5c5868dd
SeyiPerfect1 <adeegbeoluseyi@.com>
2023-01-10 01:00:53 +0100

    chore: refactored model to reflect new changes to code

--- src/models/post.models.js
+++ src/models/post.models.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-require("../models/user.models");
-const slug = require("mongoose-slug-generator");
+require('../models/user.models');
+const slug = require('mongoose-slug-generator');
 const options = {
-  separator: "-",
-  truncate: 120,
+  separator: '-',
+  truncate: 120
 };
 mongoose.plugin(slug, options);
 
@@ -13,49 +13,56 @@ const PostSchema = new Schema({
   title: {
     type: String,
     max: 75,
-    required: true,
+    required: true
   },
   description: {
     type: String,
-    max: 255,
+    max: 255
   },
   tags: [String],
   body: {
     type: String,
-    required: true,
+    required: true
   },
   readCount: {
     type: Number,
-    default: 0,
+    default: 0
   },
   readingTime: {
     type: Number,
-    required: true,
+    required: true
   },
   state: {
     type: String,
-    enum: ["draft", "published"],
-    default: "draft",
+    enum: ['draft', 'published'],
+    default: 'draft'
   },
   publishedAt: {
-    type: Date,
+    type: Date
   },
   slug: {
     type: String,
-    slug: ["title", "_id"],
+    slug: ['title', '_id'],
     unique: true,
-    lowercase: true,
+    lowercase: true
   },
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comments'
+    }
+  ],
   createdAt: {
     type: Date,
-    default: Date.now,
+    default: Date.now
   },
   updatedAt: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
-//index to fields that are not unique but will be used for ordering  for querying
+
+//  index to fields that are not unique but will be used for ordering  for querying
 PostSchema.index({
   author: 1,
   title: 1,
@@ -63,16 +70,15 @@ PostSchema.index({
   readCount: 1,
   readingTime: 1,
   state: 1,
-  publishedAt: 1,
+  publishedAt: 1
 });
 
-//presave hook to set description to title if description is not provided
-PostSchema.pre("save", function (next) {
+//  presave hook to set description to title if description is not provided
+PostSchema.pre('save', function (next) {
   if (!this.description) {
-    this.description = this.get("title");
+    this.description = this.get('title');
   }
   next();
 });
 
-
-module.exports = mongoose.model("Posts", PostSchema);
+module.exports = mongoose.model('Posts', PostSchema);
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,87 +1,94 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
-//Define user Schema
+//  Define user Schema
 const UserSchema = new Schema(
   {
     firstname: {
       type: String,
-      required: true,
+      required: true
     },
     lastname: {
       type: String,
-      required: true,
+      required: true
     },
     username: {
       type: String,
-      unique: [true, "username already exist!!!"],
-      min: [3, "username cannot be lesser than 3characters, got {value}"],
-      max: [15, "username cannot be more than 15characters, got {value}"],
-      required: true,
+      unique: [true, 'username already exist!!!'],
+      min: [3, 'username cannot be lesser than 3characters, got {value}'],
+      max: [15, 'username cannot be more than 15characters, got {value}'],
+      required: true
     },
     email: {
       type: String,
-      required: [true, "email is required"],
-      unique: [true, "user already exists!!!"],
-      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/],
+      required: [true, 'email is required'],
+      unique: [true, 'user already exists!!!'],
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/]
     },
     password: {
       type: String,
       required: true,
       match: [
         /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/,
-        "password must contain, at least a capital letter, at least a small letter, at must be at least 8 characters long",
-      ],
+        'password must contain, at least a capital letter, at least a small letter, at must be at least 8 characters long'
+      ]
     },
     intro: {
-      //brief introduction of the Author to be displayed on each post
+      //  brief introduction of the Author to be displayed on each post
       type: String,
-      max: 255,
+      max: 255
     },
     urlToImage: {
-      type: String,
+      type: String
     },
     posts: [
       {
         type: Schema.Types.ObjectId,
-        ref: "Posts",
-      },
+        ref: 'Posts'
+      }
+    ],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Posts'
+      }
     ],
     createdAt: {
       type: Date,
-      default: Date.now,
+      default: Date.now
     },
     updatedAt: {
       type: Date,
-      default: Date.now,
-    },
+      default: Date.now
+    }
   },
   {
     toJSON: {
       virtuals: true,
       transform: function (doc, ret) {
-        ret._id = ret.id
+        ret._id = ret.id;
         delete ret.password;
         delete ret.__v;
         return ret;
-      },
-    },
+      }
+    }
   }
 );
+
 UserSchema.index({
   posts: 1
-})
+});
 
-//method to hash password beefore saving to database
-UserSchema.pre("save", async function (next) {
+//  method to hash password beefore saving to database
+UserSchema.pre('save', async function (next) {
   const user = this;
 
-  //if password is modified, do nothing
-  if(!user.isModified("password")) return next();
+  //  if password is modified, do nothing
+  if (!user.isModified('password')) return next();
 
   this.email = this.email.toLowerCase();
-  //hash password
+  //  hash password
   const hash = await bcrypt.hash(this.password, 10);
 
   this.password = hash;
@@ -96,6 +103,6 @@ UserSchema.methods.isValidPassword = async function (password) {
   return compare;
 };
 
-const User = mongoose.model("users", UserSchema);
+const User = mongoose.model('users', UserSchema);
 
 module.exports = User;
